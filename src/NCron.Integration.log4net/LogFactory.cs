3bdcf5ef648b98708b906431892f4d4d3a66b9c5
schourode <jsr@malamute.dk>
2010-01-22 18:39:19 +0000

    Adapts log4net integration to new ILogFactory interface, and combines the two classes in a single file (for easier distribution).
    git-svn-id: http://ncron.googlecode.com/svn/trunk@92 352c818e-eda7-11de-bf31-4dec8810c1c1

--- src/NCron.Integration.log4net/LogFactory.cs
+++ src/NCron.Integration.log4net/LogFactory.cs
@@ -14,26 +14,88 @@
  * limitations under the License.
  */
 
+using System;
 using log4net;
 using log4net.Config;
-using NCron.Logging;
 
 namespace NCron.Integration.log4net
 {
     /// <summary>
-    /// Implements the <see cref="ILogFactory"/> interface using "log4net" as log provider.
+    /// Implements the <see cref="Logging.ILogFactory"/> interface using "log4net" as log provider.
     /// </summary>
-    public class LogFactory : ILogFactory
+    public class LogFactory : NCron.Logging.ILogFactory
     {
         static LogFactory()
         {
             BasicConfigurator.Configure();
         }
 
-        public NCron.Logging.ILog GetLogByName(string name)
+        public NCron.Logging.ILog GetLogForJob(NCron.ICronJob job)
         {
-            var internalLogger = LogManager.GetLogger(name);
+            var internalLogger = LogManager.GetLogger(job.GetType());
             return new LogAdapter(internalLogger);
+        }
+
+        internal class LogAdapter : NCron.Logging.ILog
+        {
+            private readonly ILog _log;
+
+            public LogAdapter(ILog log)
+            {
+                _log = log;
+            }
+
+            public void Debug(Func<string> msgCallback)
+            {
+                if (_log.IsDebugEnabled)
+                    _log.Debug(msgCallback());
+            }
+
+            public void Debug(Func<string> msgCallback, Func<Exception> exCallback)
+            {
+                if (_log.IsDebugEnabled)
+                    _log.Debug(msgCallback(), exCallback());
+            }
+
+            public void Info(Func<string> msgCallback)
+            {
+                if (_log.IsInfoEnabled)
+                    _log.Info(msgCallback());
+            }
+
+            public void Info(Func<string> msgCallback, Func<Exception> exCallback)
+            {
+                if (_log.IsInfoEnabled)
+                    _log.Info(msgCallback(), exCallback());
+            }
+
+            public void Warn(Func<string> msgCallback)
+            {
+                if (_log.IsWarnEnabled)
+                    _log.Warn(msgCallback());
+            }
+
+            public void Warn(Func<string> msgCallback, Func<Exception> exCallback)
+            {
+                if (_log.IsWarnEnabled)
+                    _log.Warn(msgCallback(), exCallback());
+            }
+
+            public void Error(Func<string> msgCallback)
+            {
+                if (_log.IsErrorEnabled)
+                    _log.Error(msgCallback());
+            }
+
+            public void Error(Func<string> msgCallback, Func<Exception> exCallback)
+            {
+                if (_log.IsErrorEnabled)
+                    _log.Error(msgCallback(), exCallback());
+            }
+
+            public void Dispose()
+            {
+            }
         }
     }
 }
