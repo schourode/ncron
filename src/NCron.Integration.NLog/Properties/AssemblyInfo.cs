9863bb3d26ee5fe4c698c612e3036adeb999ac45
David Duffett <duffett.david@gmail.com>
2013-02-25 15:40:33 +0000

    NLog LogFactory for NCron

---
+++ src/NCron.Integration.NLog/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NCron NLog integration")]
+[assembly: AssemblyDescription("Provides a log factory and log implementation for NCron based on the NLog library.")]
+
+[assembly: ComVisible(false)]
